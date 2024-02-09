135b2bd0f2136d88e83d7641a3bc517ddb768965
Narendra Jinka <134355944+Narendra-jinka@users.noreply.github.com>
2024-02-09 14:28:49 +0530

    Time: 15 ms (44.59%), Space: 44.2 MB (12.15%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -0,0 +1,98 @@
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] n) {
+        int len = n.length;
+        if(len == 0)
+            return new ArrayList();
+        
+        List<ArrayList> resList = new ArrayList<>();
+        for(int i : n)
+            resList.add(new ArrayList());
+        
+        Arrays.sort(n);
+        
+        for (int i = 0; i < len; i++){
+            List<Integer> max = new ArrayList<>();
+            for (int j = 0; j < i; j++)
+                if(n[i] % n[j] == 0 && resList.get(j).size() > max.size())
+                    max = resList.get(j);
+                
+            resList.get(i).addAll(max);
+            resList.get(i).add(n[i]);
+        }
+        
+        
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < len; i++){
+            if(resList.get(i).size() > res.size())
+                res = resList.get(i);
+        }
+        
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        if(nums.length==1){
+            ArrayList<Integer> arr=new ArrayList<>();
+            arr.add(nums[0]);
+            return arr;
+        }
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+        for(int i=0;i<nums.length-1;i++){
+            List<Integer> arr =new ArrayList<>();
+            arr.add(nums[i]);
+            map.put(nums[i],arr);
+            for(int j=i+1;j<nums.length;j++ ){
+                if(helper(nums[j],map.get(nums[i]))){
+                    map.get(nums[i]).add(nums[j]);
+                }
+            }
+        }
+            int max=Integer.MIN_VALUE;
+            int key=-1;
+            for(Map.Entry<Integer,List<Integer>> e:map.entrySet()){
+                if(e.getValue().size() >=max){
+                    max=e.getValue().size();
+                    key=e.getKey();
+                }
+                
+            }
+            return map.get(key);
+    }
+    public boolean helper(int n,List<Integer> res){
+        for(int i=0;i<res.size();i++){
+            if(res.get(i) % n !=0 && n % res.get(i) !=0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
+List<Integer> res=new ArrayList<>();
+        Arrays.sort(nums);
+        if(nums[0]==1){
+            res.add(1);
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(helper(nums[i],res)){
+               res.add(nums[i]); 
+            }
+        }
+        return res;
+        
+        
+    */
