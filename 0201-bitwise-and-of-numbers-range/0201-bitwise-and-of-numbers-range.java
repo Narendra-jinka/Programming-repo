d04c5fac3a0636a8efac95f9618318a75d7f90c3
Narendra Jinka <134355944+Narendra-jinka@users.noreply.github.com>
2024-02-21 22:07:02 +0530

    Time: 3 ms (100.00%), Space: 43.8 MB (68.77%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        
+        // Find the common prefix
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        
+        // Shift the common prefix back to the original position
+        return left << shift;
+    }
+}
