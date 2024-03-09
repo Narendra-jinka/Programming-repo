0fbea6192a6b1998e9cdba2297a1232cbefbe1fc
Narendra Jinka <134355944+Narendra-jinka@users.noreply.github.com>
2024-03-09 14:05:48 +0530

    Time: 20 ms (7.26%), Space: 64.7 MB (6.67%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -18,14 +18,50 @@ class Solution {
         }
         return false;
     }
-    public int getCommon(int[] nums1, int[] nums2) {
+    public int getCommon(int[] n1, int[] n2) {
         int res=-1;
-        for(int n:nums1){
-            if(search(n,nums2)){
+        
+        //binary search 
+        
+        /*for(int n:n1){
+            if(search(n,n2)){
                 res=n;
                 break;
             }
         }
+        return res;*/
+        
+        
+        
+        //========Apporach 2 -- 2pointers ==========
+        /*int i=0;
+        int j=0;
+        while(i<n1.length && j<n2.length){
+            if(n1[i] == n2[j]){
+                res=n1[i];
+                break;
+            }
+            else if(n1[i]>n2[j]){
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return res;*/
+        
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:n1){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:n2){
+            if(map.containsKey(i)){
+                res=i;
+                break;
+            }
+        }
         return res;
+        
     }
 }
