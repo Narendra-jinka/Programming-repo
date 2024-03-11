d2540bea7e196a4c3adf23d172b1c6932c1043d5
Narendra Jinka <134355944+Narendra-jinka@users.noreply.github.com>
2024-03-11 21:40:54 +0530

    Time: 2 ms (62.70%), Space: 41.5 MB (62.59%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,27 +1,19 @@
 class Solution {
     public String customSortString(String order, String s) {
-        HashMap<Character,String> map=new HashMap<>();
-        for(char c:s.toCharArray()){
-            if(!map.containsKey(c)){
-                map.put(c,c+"");
-            }
-            else{
-                String str=map.get(c);
-                map.put(c,str+(c+""));
-            }
+        StringBuilder result = new StringBuilder();
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
         }
-        StringBuffer sb=new StringBuffer();
-        for(char c:order.toCharArray()){
-            if(map.containsKey(c)){
-                sb.append(map.get(c));
-                map.remove(c);
+        for (char c : order.toCharArray()) {
+            if (mp.containsKey(c)) {
+                result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+                mp.remove(c);
             }
         }
-        
-        
-        for(Map.Entry<Character,String> e:map.entrySet()){
-            sb.append(e.getValue());
+        for (char c : mp.keySet()) {
+            result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
         }
-        return sb.toString();
+        return result.toString();
     }
 }
