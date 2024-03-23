0b24100f70466127ac2ddd889ad7307f336bec01
Narendra Jinka <134355944+Narendra-jinka@users.noreply.github.com>
2024-03-23 23:53:51 +0530

    Time: 2 ms (88.69%), Space: 48.6 MB (15.34%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,7 +9,7 @@
  * }
  */
 
-class Solution {
+/*class Solution {
     public void reorderList(ListNode head) {
         if (head == null || head.next == null || head.next.next == null) {
             return;
@@ -40,4 +40,45 @@ class Solution {
             cur.next=new ListNode(arr.get(i));
         }
     }
+}
+*/
+
+class Solution {
+    public ListNode midNode(ListNode head){
+        ListNode fast =  head, slow  =  head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast =  fast.next.next;
+            slow =  slow.next;
+        }
+        return slow;
+    }
+    
+    public ListNode reverse(ListNode head){
+        ListNode curr =  head, prev=  null, next = null;
+        while(curr!=null){
+            next  =  curr.next;
+            curr.next =  prev ;
+            prev =  curr;
+            curr  =  next;
+        }
+        return prev;
+    }
+
+    
+    public void reorderList(ListNode head) {
+        
+        ListNode midNode =  midNode(head);
+        ListNode nextToMid =  midNode.next;
+        midNode.next = null;
+        ListNode p2 =  reverse(nextToMid);
+        
+        ListNode p1 =  head ,p1Next;    
+        while(p1!=null && p2!=null){
+            p1Next =  p1.next;                                    
+			p1.next =  p2;
+			           
+            p1 =  p2;
+            p2=p1Next;                        
+        }                
+    }            
 }
