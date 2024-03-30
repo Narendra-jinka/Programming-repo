3112585acfeecf8af99b0c703acadc0420549b04
Narendra Jinka <134355944+Narendra-jinka@users.noreply.github.com>
2024-03-30 21:41:26 +0530

    Time: 4 ms (99.30%), Space: 46.8 MB (67.68%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,5 +1,5 @@
 class Solution{
-    public int subarraysWithKDistinct(int[] A, int K) {
+   /* public int subarraysWithKDistinct(int[] A, int K) {
         return subarraysWithAtMostKDistinct(A, K) - subarraysWithAtMostKDistinct(A, K - 1);
     }
     
@@ -25,5 +25,22 @@ class Solution{
         }
 
         return numGoodSubArr;
-    }
+    }*/
+    public int subarraysWithKDistinct(int[] nums, int k) {
+    int res = 0, sz = nums.length;
+    int[] cnt = new int[sz + 1];
+    for (int l = 0, m = 0, r = 0; r < sz; ++r) {
+        if (++cnt[nums[r]] == 1)
+            if (--k < 0) {
+                cnt[nums[m++]] = 0;
+                l = m;
+            }
+        if (k <= 0) {
+            while (cnt[nums[m]] > 1)
+                --cnt[nums[m++]];
+            res += m - l + 1;   
+        }
+    }    
+    return res;
+}
 }
