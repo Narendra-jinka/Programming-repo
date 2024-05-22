3294535d3f64c4e0356b738bb449c819c08d05f3
Narendra Jinka <134355944+Narendra-jinka@users.noreply.github.com>
2024-05-22 16:07:43 +0530

    Time: 1 ms (57.40%), Space: 42.7 MB (41.35%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,30 +2,29 @@ import java.util.*;
 
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        // Use a HashSet to avoid duplicate subsets
+       
         HashSet<List<Integer>> set = new HashSet<>();
         
-        // Add the empty subset
+       
         set.add(new ArrayList<>());
         
-        // Generate subsets
         generateSubsets(nums, 0, new ArrayList<>(), set);
         
-        // Convert the HashSet to a List and return
         return new ArrayList<>(set);
     }
     
     private void generateSubsets(int[] nums, int index, List<Integer> current, HashSet<List<Integer>> set) {
         if (index == nums.length) {
+            set.add(new ArrayList<>(current));
             return;
         }
         
-        for (int i = index; i < nums.length; i++) {
-            current.add(nums[i]);
-            set.add(new ArrayList<>(current));
-            generateSubsets(nums, i + 1, current, set);
-            current.remove(current.size() - 1); // Backtrack
-        }
+        current.add(nums[index]);
+        generateSubsets(nums, index + 1, current, set);
+        current.remove(current.size()-1);
+        generateSubsets(nums, index + 1, current, set);
+       
+        
     }
     
     
