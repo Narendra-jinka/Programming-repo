a3d06846c3c5b84074c73a46efa83a34070f36f6
Narendra Jinka <134355944+Narendra-jinka@users.noreply.github.com>
2024-05-26 15:53:42 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (100.00%) - LeetHub

---
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        /*int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        
+        for (int num : nums2) {
+            int product = num * k;
+            map.put(product, map.getOrDefault(product, 0) + 1);
+        }
+
+        for (int num : nums1) {
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (num % entry.getKey() == 0) {
+                    ans += entry.getValue();
+                }
+            }
+        }
+
+        return ans;*/
+        
+        int ans=0;
+        
+        for(int i:nums1){
+            for(int j:nums2){
+                if(i%(j*k)==0){
+                    ans++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
