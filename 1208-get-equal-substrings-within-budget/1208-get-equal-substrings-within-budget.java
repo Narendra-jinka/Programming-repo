5c19e43c73134029829ec05295fa743b865a132d
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2024-05-28 21:16:47 +0530

    Time: 5 ms (97.52%), Space: 43.1 MB (51.09%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -1,9 +1,16 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
         int[] cost=new int[s.length()];
+        
+        // here we are cal the cost to change the each char in s - char in t ....
         for(int i=0;i<s.length();i++){
             cost[i]=Math.abs(s.charAt(i)-t.charAt(i));
         }
+        
+        //after cal the costs we will find len the longest sub arry which has max len of maxCost .... so we use sliding window
+        
+        
+        
         int i=0,j=0;
         int cursum=0;
         int maxlen=0;
