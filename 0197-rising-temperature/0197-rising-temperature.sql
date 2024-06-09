2523a207d12533658ad7dcc1c9444310196e6aa5
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2024-06-10 04:07:50 +0530

    Time: 541 ms (93.48%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,10 +1,22 @@
 # Write your MySQL query statement below
 
 
-SELECT C.id FROM 
+/*SELECT C.id FROM 
 
 Weather AS C JOIN Weather AS P 
 
 ON DATEDIFF(C.recordDate,P.recordDate) =1 AND C.temperature > 
+
+p.temperature; */
+
+
+# Write your MySQL query statement below
+
+
+SELECT C.id FROM 
+
+Weather AS C , Weather AS P 
+
+WHERE DATEDIFF(C.recordDate,P.recordDate) =1 AND C.temperature > 
 
 p.temperature;
