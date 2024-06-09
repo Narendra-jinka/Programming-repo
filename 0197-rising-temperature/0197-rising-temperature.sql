7e9a78f38bf314b8432c736c9673ada6aa6eadf3
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2024-06-10 04:07:22 +0530

    Time: 723 ms (77.58%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,8 +3,8 @@
 
 SELECT C.id FROM 
 
-Weather AS C , Weather AS P 
+Weather AS C JOIN Weather AS P 
 
-WHERE DATEDIFF(C.recordDate,P.recordDate) =1 AND C.temperature > 
+ON DATEDIFF(C.recordDate,P.recordDate) =1 AND C.temperature > 
 
 p.temperature;
