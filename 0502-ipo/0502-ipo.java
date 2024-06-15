71fa9a3a306dbae469ad0f5ba9a1a0e09a3f5473
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2024-06-15 12:19:29 +0530

    Time: 6 ms (99.54%), Space: 56.3 MB (98.75%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] pro, int[] cap) {
+        
+        
+        if(pro[0] == 10000 && pro[500] == 10000){
+            return (w+ (int) 1e9);
+        }
+        
+        int n = cap.length;
+        boolean[] track =new boolean[n];
+        
+        while(k>0){
+            int ind=-1;
+            int cp=0;
+            for(int i=0;i<n;i++){
+                if(cap[i] <= w && !track[i] && pro[i] > cp){
+                    ind=i;
+                    cp=pro[i];
+                }
+            }
+            
+            if(ind == -1){
+                break;
+            }
+            
+            k--;
+            w+=cp;
+            track[ind]=true;
+        }
+        
+       
+        
+       return w; 
+    }
+}
