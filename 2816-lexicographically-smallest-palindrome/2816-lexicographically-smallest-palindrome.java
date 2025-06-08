388c46cdec58531a631d8260ac53c137527a6ef4
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2025-06-07 23:15:28 -0400

    Time: 10 ms (17.2%), Space: 45.7 MB (21.53%) - LeetHub

--- 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.java
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.java
@@ -1,22 +1,36 @@
 class Solution {
     public String makeSmallestPalindrome(String s) {
-        int n=s.length();
-        int i=0;
-        int j=n-1;
-        StringBuilder sb = new StringBuilder(s);
-        while(i<j){
-            char start = sb.charAt(i);
-            char end = sb.charAt(j);
+        // int n=s.length();
+        // int i=0;
+        // int j=n-1;
+        // StringBuilder sb = new StringBuilder(s);
+        // while(i<j){
+        //     char start = sb.charAt(i);
+        //     char end = sb.charAt(j);
             
-            if(start!=end){
-                if(start < end){
-                    sb.setCharAt(j,start);
-                }
-                else{
-                    sb.setCharAt(i,end);
-                }
+        //     if(start!=end){
+        //         if(start < end){
+        //             sb.setCharAt(j,start);
+        //         }
+        //         else{
+        //             sb.setCharAt(i,end);
+        //         }
+        //     }
+        //     i++;j--;
+        // }
+        // return sb.toString();
+        StringBuilder sb = new StringBuilder(s);
+        int i = 0, j = sb.length() - 1;
+
+        while (i < j) {
+            if (sb.charAt(i) != sb.charAt(j)) {
+                // Always pick the smaller character to make it lexicographically smallest
+                char smaller = (char) Math.min(sb.charAt(i), sb.charAt(j));
+                sb.setCharAt(i, smaller);
+                sb.setCharAt(j, smaller);
             }
-            i++;j--;
+            i++;
+            j--;
         }
         return sb.toString();
     }
