df81dbbeb4975596358fc2c0c62408832d769296
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2025-06-07 23:16:48 -0400

    Time: 6 ms (74.75%), Space: 45.8 MB (11.27%) - LeetHub

--- 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.java
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.java
@@ -1,37 +1,60 @@
+// class Solution {
+//     public String makeSmallestPalindrome(String s) {
+//         // int n=s.length();
+//         // int i=0;
+//         // int j=n-1;
+//         // StringBuilder sb = new StringBuilder(s);
+//         // while(i<j){
+//         //     char start = sb.charAt(i);
+//         //     char end = sb.charAt(j);
+            
+//         //     if(start!=end){
+//         //         if(start < end){
+//         //             sb.setCharAt(j,start);
+//         //         }
+//         //         else{
+//         //             sb.setCharAt(i,end);
+//         //         }
+//         //     }
+//         //     i++;j--;
+//         // }
+//         // return sb.toString();
+//         StringBuilder sb = new StringBuilder(s);
+//         int i = 0, j = sb.length() - 1;
+
+//         while (i < j) {
+//             if (sb.charAt(i) != sb.charAt(j)) {
+//                 // Always pick the smaller character to make it lexicographically smallest
+//                 char smaller = (char) Math.min(sb.charAt(i), sb.charAt(j));
+//                 sb.setCharAt(i, smaller);
+//                 sb.setCharAt(j, smaller);
+//             }
+//             i++;
+//             j--;
+//         }
+//         return sb.toString();
+//     }
+// }
 class Solution {
     public String makeSmallestPalindrome(String s) {
-        // int n=s.length();
-        // int i=0;
-        // int j=n-1;
-        // StringBuilder sb = new StringBuilder(s);
-        // while(i<j){
-        //     char start = sb.charAt(i);
-        //     char end = sb.charAt(j);
-            
-        //     if(start!=end){
-        //         if(start < end){
-        //             sb.setCharAt(j,start);
-        //         }
-        //         else{
-        //             sb.setCharAt(i,end);
-        //         }
-        //     }
-        //     i++;j--;
-        // }
-        // return sb.toString();
-        StringBuilder sb = new StringBuilder(s);
-        int i = 0, j = sb.length() - 1;
-
-        while (i < j) {
-            if (sb.charAt(i) != sb.charAt(j)) {
-                // Always pick the smaller character to make it lexicographically smallest
-                char smaller = (char) Math.min(sb.charAt(i), sb.charAt(j));
-                sb.setCharAt(i, smaller);
-                sb.setCharAt(j, smaller);
+    char[] arr = s.toCharArray();
+    int left = 0, right = arr.length - 1;
+    int ops = 0;
+    while (left < right) {
+        if (arr[left] != arr[right]) {
+            if (arr[left] > arr[right]) {
+                arr[left] = arr[right];
+            } else {
+                arr[right] = arr[left];
             }
-            i++;
-            j--;
+            ops++;
         }
-        return sb.toString();
+        left++;
+        right--;
     }
+    if (ops == 0) {
+        return s;
+    }
+    return new String(arr);
+}
 }
