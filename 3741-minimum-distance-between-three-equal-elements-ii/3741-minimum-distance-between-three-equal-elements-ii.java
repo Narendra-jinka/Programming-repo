b6cc8a1d99df04e8f1d627643daf495a5f0cdf19
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2026-04-11 12:18:35 +0530

    Time: 79 ms (92.74%), Space: 202.7 MB (95.53%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
@@ -1,24 +1,48 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        int res = Integer.MAX_VALUE;
+        // int res = Integer.MAX_VALUE;
 
-        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
-        }
-        for(Map.Entry<Integer, ArrayList<Integer>> entry:map.entrySet()){
-            List<Integer> indices = entry.getValue();
-            if (indices.size() < 3)
-                continue;
-            for (int i = 0; i < indices.size() - 2; i++) {
-                int dist = (indices.get(i + 1) - indices.get(i)) +
-                        (indices.get(i + 2) - indices.get(i + 1)) +
-                        (indices.get(i + 2) - indices.get(i));
-                res = Math.min(res, dist);
+        // HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        // }
+        // for(Map.Entry<Integer, ArrayList<Integer>> entry:map.entrySet()){
+        //     List<Integer> indices = entry.getValue();
+        //     if (indices.size() < 3)
+        //         continue;
+        //     for (int i = 0; i < indices.size() - 2; i++) {
+        //         int dist = (indices.get(i + 1) - indices.get(i)) +
+        //                 (indices.get(i + 2) - indices.get(i + 1)) +
+        //                 (indices.get(i + 2) - indices.get(i));
+        //         res = Math.min(res, dist);
+        //     }
+
+        // }
+        // if(res == Integer.MAX_VALUE) res = -1;
+        // return res;
+
+         Map<Integer, int[]> mp = new HashMap<>();
+        int mini = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!mp.containsKey(nums[i])) {
+                mp.put(nums[i], new int[]{-1, i});
+            } else {
+                int[] pair = mp.get(nums[i]);
+                int prev = pair[0];
+                int last = pair[1];
+
+                if (prev != -1) {
+                    int a = prev, b = last, c = i;
+                    int diff = 2 * (c - a);
+                    mini = Math.min(mini, diff);
+                }
+
+                pair[0] = last;
+                pair[1] = i;
             }
-
         }
-        if(res == Integer.MAX_VALUE) res = -1;
-        return res;
+
+        return mini == Integer.MAX_VALUE ? -1 : mini;
     }
 }
