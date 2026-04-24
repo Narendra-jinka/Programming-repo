7df043cb4364815fcc6a95ce7430d3281d446248
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2026-04-24 12:58:16 +0530

    Time: 2 ms (99.38%), Space: 44 MB (96.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         int i=0,j = s.length()-1;
         while(i<j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;j--;
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            else{
+                if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))) return false;
+                i++;j--;
+            }
+
         }
 
         return true;
