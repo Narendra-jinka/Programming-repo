2eb3211995e5579ed64d5e2c1466a9bb3d3740e7
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2026-05-18 22:46:57 +0530

    Time: 6 ms (20.24%), Space: 51.1 MB (97.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,22 +1,75 @@
-class Solution {
-    public int numIslands(char[][] grid) {
-    int count=0;
-    for(int i=0;i<grid.length;i++)
-        for(int j=0;j<grid[0].length;j++){
-            if(grid[i][j]=='1'){
-                dfsFill(grid,i,j);
-                count++;
-            }
-        }
-    return count;
-}
-private void dfsFill(char[][] grid,int i, int j){
-    if(i>=0 && j>=0 && i<grid.length && j<grid[0].length&&grid[i][j]=='1'){
-        grid[i][j]='0';
-        dfsFill(grid, i + 1, j);
-        dfsFill(grid, i - 1, j);
-        dfsFill(grid, i, j + 1);
-        dfsFill(grid, i, j - 1);
+class Pair{
+    int i;
+    int j;
+
+    public Pair(int i,int j){
+        this.i = i;
+        this.j = j;
     }
 }
+
+class Solution {
+
+    public void bfs(int i, int j, char[][] grid, int[][] vis){
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(i,j));
+
+        vis[i][j] = 1;
+
+        // 4 directions
+        int[] dr = {-1,0,1,0};
+        int[] dc = {0,1,0,-1};
+
+        while(!q.isEmpty()){
+
+            Pair p = q.poll();
+
+            for(int k=0;k<4;k++){
+
+                int nr = p.i + dr[k];
+                int nc = p.j + dc[k];
+
+                if(nr >= 0 && nr < n &&
+                   nc >= 0 && nc < m &&
+                   vis[nr][nc] == 0 &&
+                   grid[nr][nc] == '1'){
+
+                    vis[nr][nc] = 1;
+
+                    q.offer(new Pair(nr,nc));
+                }
+            }
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][] vis = new int[n][m];
+
+        int count = 0;
+
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<m;j++){
+
+                if(vis[i][j] == 0 &&
+                   grid[i][j] == '1'){
+
+                    bfs(i,j,grid,vis);
+
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 }
