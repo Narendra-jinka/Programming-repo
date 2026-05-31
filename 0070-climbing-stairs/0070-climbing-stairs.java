22463c74b19475fd3133195e80b82cf3ad0f3732
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2026-05-31 21:21:35 +0530

    Time: 44 ms (0.27%), Space: 41.8 MB (85.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,27 +1,18 @@
 class Solution {
-    private static int f(int ind,int[] dp){
-        if (ind <= 1) return 1;
-        
-        if(dp[ind]!=-1) return dp[ind];
-        
-        return dp[ind]=f(ind-1,dp)+f(ind-2,dp);
+    public int f(int n,int[] dp){
+        if(n<0) return 0;
+        if(dp[n]!=0) return dp[n];
+        int left = f(n-1,dp);
+        int right = f(n-2,dp);
+        return dp[n] = left+right;
     }
     public int climbStairs(int n) {
-        /*int[] dp = new int[n + 1];
-        dp[0] = dp[1] = 1;
+        int[] dp = new int[n+1];
+        dp[0]=1;
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        return dp[n];*/
-        
-        if(n<=1) return 1;
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
         f(n,dp);
+
         return dp[n];
         
-        
-    }    
+    }
 }
