a3d89dfad5b54937d089d72b121891f3b770f39a
C0d3CipheR <134355944+Narendra-jinka@users.noreply.github.com>
2026-06-05 00:24:01 +0530

    Time: 7 ms (10.44%), Space: 50.1 MB (70.55%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -13,7 +13,24 @@ class Solution {
         int n = g[0].length;
         if(m==1 && n==1) return g[0][0];
         int[][] dp = new int[m][n];
-        helper(m-1,n-1,g,dp);
+        // helper(m-1,n-1,g,dp);
+
+        dp[0][0] = g[0][0];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 && j== 0) dp[0][0] = g[0][0];
+                else{
+                    dp[i][j] += g[i][j];
+                    int up = Integer.MAX_VALUE;
+                    int left = Integer.MAX_VALUE;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j]+=Math.min(up,left);
+                }
+            }
+        }
         return dp[m-1][n-1];
     }
 }
